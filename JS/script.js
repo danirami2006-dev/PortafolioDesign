1d032f1367d425786ff0d94e68bcc17cb5f5c256
Daniela Quintero Ramirez <dani.rami2006@gmail.com>
2026-08-08 08:36:00 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -6,8 +6,8 @@ const videoSource = document.getElementById('videoSource');
 
 // Lista con la ruta de tus dos partes
 const partesVideo = [
-    'Videos/bannerpieza1.mp4',
-    'Videos/bannerpieza2.mp4'
+    'video/bannerpieza1.mp4',
+    'video/bannerpieza2.mp4'
 ];
 
 let indiceActual = 0;
